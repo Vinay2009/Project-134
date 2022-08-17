33458d6c4807c3f0cce355f00c69c9ac1aad3ea4
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-08-17 19:56:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+status="";
+objects = [];
+alarm = "";
+
+function start() {
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 300);
+    video.hide();
+}
+
+function preload() {
+    alarm = loadSound("NBUK26X-alarm.mp3");
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+    }
+
+function draw()
+{
+ image(video, 0, 0, 380, 380);
+ 
+ if(status != "");
+ { 
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    for (i = 0; i < objects.length; i++) { 
+        document.getElementById("status").innerHTML = "Status : Object Detected"; 
+        fill(r,g,b);
+         percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+           noFill();
+            stroke(r,g,b);
+             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+              if(objects[i].label == "person") { 
+                document.getElementById("number_of_objects").innerHTML = "Baby Found";
+                 console.log("stop");
+                  alarm.stop(); 
+                }
+                 else { 
+                    document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+                    console.log("play");
+                     alarm.play(); 
+                    } 
+                } 
+                    if(objects.length == 0) {
+                         document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+                    console.log("play"); 
+                    alarm.play();
+
+ }
+}
+}
